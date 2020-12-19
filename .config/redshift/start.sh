704b2291fcf0794bed80efd17299c386f9a6722a
Adam Krška <adam@krska.com>
2020-12-19 23:13:00 +0100

    [redshift] added

---
+++ .config/redshift/start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Terminate already running instances
+killall -q redshift
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x redshift >/dev/null; do sleep 1; done
+
+# Clean changes
+redshift -x
+
+# Start redshift
+redshift &
